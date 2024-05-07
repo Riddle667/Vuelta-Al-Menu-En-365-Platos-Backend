0de29fd36251c1afb99f0cb484118014853ffbd3
Erik Maluenda <114884171+soberlewa12@users.noreply.github.com>
2024-05-07 19:58:43 -0400

    Update uploadControlller.js

--- controllers/uploadControlller.js
+++ controllers/uploadControlller.js
@@ -3,10 +3,10 @@ const User = require("../models/user");
 const cloudinary = require('cloudinary').v2;
 
 const updateImageCloudinary = async (req = request, res = response) => {
+
     try {
         const { collection, id } = req.params;
         let model;
-
         switch (collection) {
             case 'users':
                 model = await User.findByPk(id);
