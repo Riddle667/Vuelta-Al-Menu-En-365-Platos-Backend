2fc75fdf40d25443ff218c9ba959b687b7ec6622
Erik Maluenda <114884171+soberlewa12@users.noreply.github.com>
2024-05-16 17:44:15 -0400

    <feat> Cambios en el changePassword

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { request, response } = require("express")
 const User = require("../models/user")
 const emailHelper = require('../helpers/send-email');
+const bcryptjs = require("bcryptjs");
 
 const getUsers = async (req = request, res = response) => {
     try {
@@ -16,32 +17,42 @@ const getUsers = async (req = request, res = response) => {
 }
 
 const changePassword = async (req = request, res = response) => {
-    const { email } = req.body;
+    const { userId, newPassword } = req.body;
 
-    console.log(email);
-    const user = await User.findOne({ where: { email } });
+    try {
+        const user = await User.findByPk(userId);
 
-    if (!user) {
-        return res.status(400).json({
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: 'User is not valid'
+            });
+        }
+
+        // Hash the new password
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync(newPassword, salt);
+        
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Password changed successfully'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
             success: false,
-            message: 'User is not valid'
-        })
+            message: 'Server error'
+        });
     }
-
-    console.log(user);
-    // make verification code
-    const verificationCode = 123489;
-
-    await emailHelper.sendEmail(
-        user.email,
-        `Código de verificación por correo electrónico: ${verificationCode}`,
-        `Estimad@ ${user.name} ${user.lastName} para modificar su contraseña debe ingresar el siguiente código de verificación`,
-        `<b>${verificationCode}</b>`
-    );
-
-    res.status(200).send('Email Send');
-
 }
+
+module.exports = {
+    getUsers,
+    changePassword
+}
+
 
 module.exports = {
     getUsers,
